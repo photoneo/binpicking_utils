0af0cb755727092c89b5c1bd9b5e08574e0cfed6
Michal Dobis <miso.dobis@gmail.com>
2018-11-11 09:23:18 +0100

    Visiulize poses from statistics file

--- bin_pose_emulator/src/visualizer.cpp
+++ bin_pose_emulator/src/visualizer.cpp
@@ -22,129 +22,181 @@ limitations under the License.
 
 #include <boost/algorithm/string/classification.hpp> // Include boost::for is_any_of
 #include <boost/algorithm/string/split.hpp> // Include for boost::split
-#include <visualization_msgs/MarkerArray.h>
 
-class Visualizer : public BinPoseEmulator{
+#include <sensor_msgs/PointCloud.h>
+#include <bin_pose_msgs/change_data.h>
+
+#include <mutex>
+
+class Visualizer{
 public:
 
-    Visualizer(ros::NodeHandle* nh, std::string filepath) : BinPoseEmulator(nh, filepath) {
+    Visualizer(ros::NodeHandle* nh, std::string filepath){
 
-        /*uint32_t shape = visualization_msgs::Marker::ARROW;
-
-        marker_.ns = "bin";
-        marker_.id = 1;
-        marker_.type = shape;
-        marker_.action = visualization_msgs::Marker::ADD;
-
-        marker_.scale.x = 0.01;
-        marker_.scale.y = 0.02;
-        marker_.scale.z = 0.05;
-
-        marker_.color.r = 0.9f;
-        marker_.color.g = 0.9f;
-        marker_.color.b = 0.0f;
-        marker_.color.a = 1.0;*/
-
-    }
-
-    void visualize(){
-
-        geometry_msgs::Pose pose;
-        geometry_msgs::Pose approach;
+        point_cloud_pub_ = nh->advertise<sensor_msgs::PointCloud>("point_cloud",10);
+        displayed_value_ = 4;
 
         std::ifstream infile;
 
         std::string str;
 
-        infile.open("/home/controller/catkin_ws/fails.txt",  std::ifstream::in);
+        infile.open("/home/controller/catkin_ws/planner_test/points_test_VW.txt",  std::ifstream::in);
 
+        cloud_.points.clear();
+        cloud_.channels.resize(3);
+
+        ROS_WARN("visualizer start");
         while (!infile.eof()) {
 
             //position
             std::getline(infile, str);
-            std::getline(infile, str);
-            pose.position.x = getDouble(str);
-            std::getline(infile, str);
-            pose.position.y = getDouble(str);
-            std::getline(infile, str);
-            pose.position.z = getDouble(str);
+            data_.push_back(parseLine(str));
+        }
 
-            // orientation
-            std::getline(infile, str);
-            std::getline(infile, str);
-            pose.orientation.x = getDouble(str);
-            std::getline(infile, str);
-            pose.orientation.y = getDouble(str);
-            std::getline(infile, str);
-            pose.orientation.z = getDouble(str);
-            std::getline(infile, str);
-            pose.orientation.w = getDouble(str);
+        data_.erase(data_.end());
 
-            // empty line
-            std::cout << pose << "\n";
-            std::getline(infile, str);
+        ROS_INFO("Size of values %d", data_[0].size());
+        int max_value = 0;
+        for (int i = 4; i < 14; i++){
+            displayed_value_ = i;
+            max_value = findMaxValue();
+            ROS_INFO("Maximum fails [%s] on 1 point %d", data_types_[i].c_str(), i);
+        }
 
-            approach = pose;
-            approach.position.z += 0.1;
-            visualizeBin();
-            visualizePose(pose, approach, true);
-
+        displayed_value_ = 4;
+        max_value = findMaxValue();
+        for (auto &values : data_){
+            parseValues(cloud_, values, max_value);
         }
     }
 
+    void visualize(){
+
+        mutex_.lock();
+        cloud_.header.stamp = ros::Time::now();
+        cloud_.header.frame_id = "base_link";
+        cloud_.header.seq++;
+        point_cloud_pub_.publish(cloud_);
+        mutex_.unlock();
+   }
+
+    bool changeDataCallback(bin_pose_msgs::change_data::Request &req, bin_pose_msgs::change_data::Response &res){
+
+        displayed_value_ = req.data;
+
+        int sum_of_failures = 0;
+        mutex_.lock();
+        res.max_failures_on_point = findMaxValue();
+
+        for (auto &values : data_){
+            sum_of_failures += parseValues(cloud_, values, res.max_failures_on_point);
+        }
+
+        res.message = "Type " + data_types_[displayed_value_] + " has " + std::to_string(sum_of_failures) + " fails";
+        mutex_.unlock();
+
+        return true;
+    }
 private:
 
-    //visualization_msgs::Marker marker_;
+    ros::Publisher point_cloud_pub_;
+    sensor_msgs::PointCloud cloud_;
+    int displayed_value_;
+    std::vector<std::vector<double> > data_;
+    std::mutex mutex_;
 
+    std::map<int, std::string> data_types_= {
+            {0, "ID"},
+            {1,  "X"},
+            {2,  "Y"},
+            {3,  "Z"},
+            {4,  "Grasp IK"},
+            {5,  "Approach IK"},
+            {6,  "Deapproach IK"},
+            {7,  "start - approach J traj"},
+            {8,  "aprroach - grasp L traj"},
+            {9,  "grasp - deaprroach L traj"},
+            {10, "deaprroach - end J traj"},
+            {11,  "start - approach continuity"},
+            {12,  "aprroach - grasp continuty"},
+            {13,  "grasp - deaprroach continuity"},
+            {14, "deaprroach - end continuity"},
+    };
 
-    double getDouble(std::string str){
+    std::vector<double> parseLine(std::string str){
+        ROS_INFO("%s", str.c_str());
+        std::vector<double> values;
         std::vector<std::string> words;
-        boost::split(words, str, boost::is_any_of(":"), boost::token_compress_on);
-        return (double)atof(words[1].c_str());
+        boost::split(words, str, boost::is_any_of(","), boost::token_compress_on);
+
+        for (const auto &word : words) {
+           values.push_back((double) atof(word.c_str()));
+        }
+        return values;
     }
 
-   /*void visualizePose(){
+    int parseValues(sensor_msgs::PointCloud &cloud, const std::vector<double> &values, int max_intesity_value){
 
-        marker_.header.frame_id = "/base_link";
-        marker_.header.stamp = ros::Time::now();
-      //  marker.lifetime = ros::Duration();
-      //  marker_pub_.publish(marker);
+        geometry_msgs::Point32 p;
+        sensor_msgs::ChannelFloat32 channel;
+
+        p.x = values[1];
+        p.y = values[2];
+        p.z = values[3];
+
+        double scale = 1.0/(max_intesity_value/2.0);
+        float r = values[displayed_value_]*scale;
+        float g = 0.0;
+        float b = 2.0 - values[displayed_value_]*scale;
+
+        cloud.channels[0].name = "r";
+        cloud.channels[1].name = "g";
+        cloud.channels[2].name = "b";
+        cloud.channels[0].values.push_back(r);
+        cloud.channels[1].values.push_back(b);
+        cloud.channels[2].values.push_back(g);
+        cloud.points.push_back(p);
+
+        return values[displayed_value_];
     }
 
-    void addPose(geometry_msgs::Pose grasp_pose,
-                                  geometry_msgs::Pose approach_pose)
-    {
-        geometry_msgs::Point approach_point;
-        approach_point.x = approach_pose.position.x;
-        approach_point.y = approach_pose.position.y;
-        approach_point.z = approach_pose.position.z;
+    int findMaxValue(){
 
-        geometry_msgs::Point grasp_point;
-        grasp_point.x = grasp_pose.position.x;
-        grasp_point.y = grasp_pose.position.y;
-        grasp_point.z = grasp_pose.position.z;
-
-        marker_.points.push_back(approach_point);
-        marker_.points.push_back(grasp_point);
-    }*/
-
+        int max_value = 0;
+        for (auto &values : data_){
+            if (values[displayed_value_] > max_value)
+                max_value = values[displayed_value_];
+        }
+        return max_value;
+    }
 };
 
 
 int main(int argc, char* argv[])
 {
-  ros::init(argc, argv, "bin_pose_emulator");
+  ros::init(argc, argv, "visualizer");
   ros::NodeHandle nh;
 
   // Get config filepath from ROS Param server
   std::string filepath;
   nh.getParam("filepath", filepath);
 
-    sleep(15);
+    // Start Async Spinner with 2 threads
+    ros::AsyncSpinner spinner(2);
+    spinner.start();
 
+    sleep(1);
     Visualizer visualizer(&nh, filepath);
-    visualizer.visualize();
 
+    ros::ServiceServer change_data_service = nh.advertiseService("change_data", &Visualizer::changeDataCallback, &visualizer);
+
+
+    ros::Rate rate(1);
+
+    while (ros::ok()){
+        visualizer.visualize();
+        rate.sleep();
+    }
 
   ROS_WARN("creating service");
   ros::spin();
