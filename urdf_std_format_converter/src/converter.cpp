31ea1e1d9559f1241d04093344af40540f110101
Lukas Stehlik <stehlik@photoneo.com>
2019-07-26 16:50:00 +0200

    Data prepared for write to XML file

--- urdf_std_format_converter/src/converter.cpp
+++ urdf_std_format_converter/src/converter.cpp
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include <Eigen/Dense>
 #include <urdf/model.h>
-//#include "../../../../../../../opt/ros/kinetic/include/urdf/model.h"
+#include <tinyxml.h>
 
 
 class RobotModel : public urdf::Model {
@@ -22,35 +22,15 @@ public:
         }
         return joints;
     }
-};
 
-class Matrix4x4d : public Eigen::Matrix4d {
-public:
-    static Eigen::Matrix4d RotMat_X(double a){
-        Eigen::Matrix4d mat;
-        mat <<  1, 0, 0, 0,
-                0, std::cos(a), -std::sin(a), 0,
-                0, std::sin(a), std::cos(a), 0,
-                0, 0, 0, 1;
-        return mat;
-    }
-
-    static Eigen::Matrix4d RotMat_Y(double a){
-        Eigen::Matrix4d mat;
-        mat <<  std::cos(a), 0, std::sin(a), 0,
-                0, 1, 0, 0,
-                -std::sin(a), 0, std::cos(a), 0,
-                0, 0, 0, 1;
-        return mat;
-    }
-
-    static Eigen::Matrix4d RotMat_Z(double a){
-        Eigen::Matrix4d mat;
-        mat <<  std::cos(a), -std::sin(a), 0, 0,
-                std::sin(a), std::cos(a), 0, 0,
-                0, 0, 1, 0,
-                0, 0, 0, 1;
-        return mat;
+    std::vector<urdf::LinkSharedPtr> getChildLinks() {
+        std::vector<urdf::LinkSharedPtr> links;
+        urdf::LinkConstSharedPtr actual_link = getRoot();
+        for (int i = 0; i < links_.size() - 2; i++) {
+            links.push_back(actual_link->child_links[0]);
+            actual_link = actual_link->child_links[0];
+        }
+        return links;
     }
 };
 
@@ -70,13 +50,22 @@ int main(int argc, char **argv) {
     model.initFile("/home/controller/catkin_ws/src/" + robot + "/" + robot + "_support/urdf/robot.urdf");
 
     auto joints = model.getChildJoints();
+    auto links = model.getChildLinks();
 
-    std::vector<Eigen::Matrix4d> MatR; // Rotational matrices
-    std::vector<Eigen::Matrix4d> MatT; // Translational matrices
-    for (int i = 0; i < joints.size(); i++) {
-        auto joint = joints[i];
-        //if (joint->type != urdf::Joint::REVOLUTE) continue;
+    std::vector<Eigen::Matrix4d> LMatR; // Link Rotational matrices
+    for (const auto &link : links) {
+        Eigen::Quaterniond quat;
+        link->visual->origin.rotation.getQuaternion(quat.x(), quat.y(), quat.z(), quat.w());
+        std::cout << link->name << std::endl;
+        Eigen::Matrix4d temp = Eigen::Matrix4d::Identity();
+        temp.block<3, 3>(0, 0) = quat.toRotationMatrix();
+        std::cout << quat.toRotationMatrix().eulerAngles(2, 1, 0) << "\n\n";
+        LMatR.push_back(temp);
+    }
 
+    std::vector<Eigen::Matrix4d> JMatR; // Joint Rotational matrices
+    std::vector<Eigen::Matrix4d> JMatT; // Joint Translational matrices
+    for (const auto &joint : joints) {
         double x = joint->parent_to_joint_origin_transform.position.x;
         double y = joint->parent_to_joint_origin_transform.position.y;
         double z = joint->parent_to_joint_origin_transform.position.z;
@@ -85,64 +74,74 @@ int main(int argc, char **argv) {
         temp << Eigen::Matrix3d::Identity(), Eigen::Vector3d(x, y, z),
                 0, 0, 0, 1;
 //        std::cout << temp << std::endl;
-        MatT.push_back(temp);
+        JMatT.push_back(temp);
 
 
-       /* Eigen::Quaterniond quaternion = (Eigen::Quaterniond(joint->parent_to_joint_origin_transform.rotation.w, \
-        joint->parent_to_joint_origin_transform.rotation.x, \
-        joint->parent_to_joint_origin_transform.rotation.y, \
-        joint->parent_to_joint_origin_transform.rotation.z));*/
-        Eigen::Quaterniond quaternion;
-        joint->parent_to_joint_origin_transform.rotation.getQuaternion(quaternion.x(), quaternion.y(), quaternion.z(), quaternion.w());
+        Eigen::Quaterniond quat;
+        joint->parent_to_joint_origin_transform.rotation.getQuaternion(quat.x(), quat.y(), quat.z(), quat.w());
         temp = Eigen::Matrix4d::Identity();
-        temp.block<3, 3>(0, 0) = quaternion.toRotationMatrix();
-        std::cout << quaternion.toRotationMatrix().eulerAngles(0, 1, 2) << "\n\n";
-        std::cout << quaternion.vec() << "\n" << quaternion.w() << std::endl;
+        temp.block<3, 3>(0, 0) = quat.toRotationMatrix();
+        std::cout << quat.toRotationMatrix().eulerAngles(2, 1, 0) << "\n\n";
+//        std::cout << quaternion.vec() << "\n" << quaternion.w() << std::endl;
 //        std::cout << temp << std::endl << std::endl;
 
-//        joint->parent_to_joint_origin_transform.rotation.getRPY(x, y, z);
-//        std::cout << x << std::endl;
-//        std::cout << y << std::endl;
-//        std::cout << z << std::endl;
-//        std::cout << Matrix4x4d::RotMat_X(x) * Matrix4x4d::RotMat_Y(y) * Matrix4x4d::RotMat_Z(z) << std::endl;
-//        temp = Matrix4x4d::RotMat_X(x) * Matrix4x4d::RotMat_Y(y) * Matrix4x4d::RotMat_Z(z);
-        MatR.push_back(temp);
+        JMatR.push_back(temp);
     }
 
-    std::cout << "---Joint offsets---" << std::endl;
+    std::vector<Eigen::Vector3d> newJointRPYs;
+    std::vector<Eigen::Vector3d> newJointOffsets;
+    std::vector<Eigen::Vector3d> newJointAxes;
+    std::vector<Eigen::Vector3d> newLinkRPYs;
+
+    std::cout << "---Joint Offsets---" << std::endl;
     Eigen::Matrix4d mat = Eigen::Matrix4d::Identity();
     for (int i = 0; i < joints.size(); i++) {
         Eigen::Vector4d nullvec;
         nullvec << 0, 0, 0, 1;
+        newJointOffsets.emplace_back(((mat * JMatT[i]) * Eigen::Vector4d(0, 0, 0, 1)).head(3));
         std::cout << joints[i]->name << std::endl;
-        std::cout << ((mat * MatT[i]) * nullvec).head(3) << std::endl << std::endl;
-        mat = mat * MatR[i];
-        if (i == joints.size() - 1) {
-            std::cout << mat.block<3, 3>(0, 0).eulerAngles(0, 1, 2) << std::endl;
-            std::cout << mat << std::endl;
+        std::cout << newJointOffsets[i] << std::endl << std::endl;
+        mat *= JMatR[i];
+        if (i < joints.size() - 1) {
+            newJointRPYs.emplace_back(Eigen::Vector3d::Zero());
+        } else {
+            newJointRPYs.push_back(mat.block<3, 3>(0, 0).eulerAngles(2, 1, 0));
+            std::cout << newJointRPYs[i] << std::endl;
+//            std::cout << mat << std::endl;
         }
     }
 
-    std::cout << "---Joint Axis---" << std::endl;
+    std::cout << "---Joint Axes---" << std::endl;
     mat = Eigen::Matrix4d::Identity();
     for (int i = 0; i < joints.size() - 1; i++) {
         Eigen::Vector4d axis;
         axis = Eigen::Vector4d(joints[i]->axis.x, joints[i]->axis.y, joints[i]->axis.z, 1);
 
-        mat = mat * MatR[i];
+        mat *= JMatR[i];
+        newJointAxes.emplace_back((mat * axis).head(3));
+
+        double eps = 0.01;
+        for (int j = 0; j < 3; j++) {
+            double absval = std::round(newJointAxes[i](j));
+            if ((absval < eps) || (std::abs(absval - 1) < eps)) {
+                newJointAxes[i](j) = round(newJointAxes[i](j));
+            }
+        }
+        newJointAxes[i](0) = std::round(newJointAxes[i](0));
         std::cout << joints[i]->name << std::endl;
-        std::cout << (mat * axis).head(3) << std::endl << std::endl;
+        std::cout << newJointAxes[i] << std::endl << std::endl;
     }
 
+    std::cout << "---Link RPYs---" << std::endl;
+    mat = Eigen::Matrix4d::Identity();
+    for (int i = 0; i < links.size(); i++) {
+        mat *= JMatR[i];
+        newLinkRPYs.push_back((mat * LMatR[i]).block<3, 3>(0, 0).eulerAngles(2, 1, 0));
+        std::cout << links[i]->name << std::endl;
+        std::cout << newLinkRPYs[i] << std::endl << std::endl;
+    }
 
-//    Eigen::Matrix<double, 4, 4> mat(Eigen::Matrix4d::Identity());
-//    Eigen::Matrix4d mat2 = Eigen::Matrix4d::Identity();
-//
-//
-//    mat * mat2;
-//    mat = Eigen::Matrix4d::Identity();
-//    Eigen::Vector4d vec = Eigen::Vector4d::Ones();
-//    mat * vec;
-//    std::cout << mat << std::endl;
-//    std::cout << "Hello world" << std::endl;
+    //TiXmlDocument XacroXML = TiXmlDocument("/home/controller/catkin_ws/src/" + robot + "/" + robot + "_support/urdf/robot_macro.xacro");
+    //XacroXML.LoadFile("/home/controller/catkin_ws/src/" + robot + "/" + robot + "_support/urdf/robot_macro.xacro.urdf");
+    //std::cout << XacroXML << std::endl;
 }
